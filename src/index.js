546c255766e182c41fae6d06eaec40b5a2b73ceb
sigizmunde <1816579@gmail.com>
2024-04-13 22:58:17 +0300

    Add canvas sample text

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import "./scss/styles.scss";
+
+const canvas = document.getElementById("main-canvas");
+const ctx = canvas.getContext("2d");
+
+ctx.font = "bold 48px serif";
+ctx.fillText("#@)₴?$0", 50, 100);
